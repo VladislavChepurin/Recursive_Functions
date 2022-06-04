ed6293fe4c156da73e496be119b33837b4e21833
Vladislav <vlad1988_1@mail.ru>
2022-06-04 18:48:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Напишите что-то");
+
+		string str = Console.ReadLine();
+
+		Console.WriteLine("Укажите глубину эха");
+
+		int deep = int.Parse(Console.ReadLine() ?? "0");
+
+		Echo(str, deep);
+
+	}
+
+	static void Echo(string saidword, int deep)
+	{
+		Console.WriteLine(saidword);
+		if (saidword.Length > 2)
+		{
+			saidword = saidword.Remove(0, 2);
+		}
+
+		if (deep > 1)
+		{
+			Echo(saidword, --deep);
+		}
+	}
+}
